343b8fb3a900b649634af9c19ab1642db81349c5
Pavel Zaitsev <pavel.v.zaitsev@gmail.com>
2019-07-16 13:06:55 +0200

    csrf header setting for token check now - works

--- app/javascript/components/UploadVideo.js
+++ app/javascript/components/UploadVideo.js
@@ -3,6 +3,7 @@ import React from 'react'
 class UploadVideo extends React.Component {
     triggerUpload(e, item){
         console.log(e.target.files)
+        console.log(e.target.form['authenticity_token'])
         let file = e.target.files[0]
         let form = new FormData()
         form.append("video[video]", file)
@@ -11,7 +12,9 @@ class UploadVideo extends React.Component {
         fetch("/videos", {
             method: 'POST',
             body: form,
-            headers: {}
+            headers: {
+                "X-CSRF-Token": e.target.form['authenticity_token'].value
+            }
         })
             .then(response => response.json(), error => console.log("An error occured", error))
             .then(json => console.log("Successfuly uploaded"))
@@ -24,6 +27,7 @@ class UploadVideo extends React.Component {
             <React.Fragment>
                 <form action="/videos" method="post">
                     <input type="hidden" name="authenticity_token" value={token} readOnly={true} />
+
                      FILE:<br/>
                      <input key="one" id="upload" type="file" onChange={this.triggerUpload.bind(this)}/>
                 </form>
