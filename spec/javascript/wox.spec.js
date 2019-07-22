719ba960620e879a8bebdcaa81a05596d820c4ae
Pavel Zaitsev <pavel.v.zaitsev@gmail.com>
2019-07-22 14:16:28 +0200

    got tests working.react tests generating snapshots on components./

--- spec/javascript/wox.spec.js
+++ spec/javascript/wox.spec.js
@@ -1,4 +1,9 @@
-// wat
+import React from 'react'
+import Button from 'Button'
+import renderer from 'react-test-renderer'
+
 test('1 + 1 equals 2', () => {
-  expect(1 + 1).toBe(2);
+    const component = renderer.create(<Button name="Untitled" title="Hello"/>)
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
 });
