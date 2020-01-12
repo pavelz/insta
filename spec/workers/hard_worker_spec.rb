f19a41bda48552760c6922f9e3c400027febc7c8
Pavel Zaitsev <pzaitsev-cf@dnanexus.com>
2020-01-12 04:51:40 +0300

    this test costs moeny - commented out

--- spec/workers/hard_worker_spec.rb
+++ spec/workers/hard_worker_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 RSpec.describe HardWorker, type: :worker do
   describe "Hard worker should" do
     it "should do something" do
-      Location.create!({lng: 30.308963, lat: 59.985897})
-      subject.perform(Location.first.id)
-      expect(Location.first.address).not_to be_blank
+      #Location.create!({lng: 30.308963, lat: 59.985897})
+      #subject.perform(Location.first.id)
+      #expect(Location.first.address).not_to be_blank
     end
   end
 end
