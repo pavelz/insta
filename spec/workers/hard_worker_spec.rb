2c6ebc2713e7b0f2d7bce5b050dceae78c5bcfa4
Pavel Zaitsev <pzaitsev-cf@dnanexus.com>
2020-01-12 04:42:42 +0300

    fixed geolocation test

--- spec/workers/hard_worker_spec.rb
+++ spec/workers/hard_worker_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 RSpec.describe HardWorker, type: :worker do
-  it "should do something" do
-    subject.perform()
+  describe "Hard worker should" do
+    it "should do something" do
+      Location.create!({lng: 30.308963, lat: 59.985897})
+      subject.perform(Location.first.id)
+      expect(Location.first.address).not_to be_blank
+    end
   end
 end
