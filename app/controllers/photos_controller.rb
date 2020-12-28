4d67a58a2403c210f42eff7a067c89f5943b5ae5
Pavel Zaitsev <pavel@arslogi.ca>
2020-12-27 20:22:17 -0500

    fix a tiny permission issuse

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,8 @@
 class PhotosController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
-  acts_as_token_authentication_handler_for User, except: [:index], if: lambda { |controller| controller.request.format.json? }
-  before_action :authenticate_user!, if: lambda { |controller| controller.request.format.html? }
+  acts_as_token_authentication_handler_for User, except:[:index], if: lambda { |controller| controller.request.format.json? }
+  before_action :authenticate_user!, unless: lambda { |controller| controller.request.format.json? }
   skip_before_action :authenticate_user!, only: [:create, :index]
 
   MAX_FEED = 100
