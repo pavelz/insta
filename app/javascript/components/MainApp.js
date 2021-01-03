692757bf12c4f936b2f610146365dfe91527c62a
Pavel Zaitsev <pavel@arslogi.ca>
2021-01-03 15:01:45 -0500

    playing with stylesheet packs, also coc reformatted some of my jsx files 😆

--- app/javascript/components/MainApp.js
+++ app/javascript/components/MainApp.js
@@ -1,80 +1,84 @@
-import React from "react"
-import PropTypes from "prop-types"
-import {fetchPhotos} from "./PhotoActions"
+import React from "react";
+import PropTypes from "prop-types";
+import { fetchPhotos } from "./PhotoActions";
 
-import {connect} from  'react-redux'
-import Photos from "./Photos"
-import Video from "./Video"
-import About from "./About"
-import {BrowserRouter as Router, Route, Link} from "react-router-dom"
+import { connect } from "react-redux";
+import Photos from "./Photos";
+import Video from "./Video";
+import About from "./About";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import { Grid, Row  } from 'react-flexbox-grid';
-
+import { Grid, Row } from "react-flexbox-grid";
 
 class MainApp extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
   componentDidMount() {
-    const {dispatch} = this.props;
-    dispatch(fetchPhotos(1))
+    const { dispatch } = this.props;
+    dispatch(fetchPhotos(1));
   }
-   render(){
-        const {photos} = this.props;
-        return(
-            <React.Fragment>
-                <Router>
-                    <Grid fluid>
-                        <Row className="center-xs" style={{'justify-content': "center", "text-align": 'center'}}>
-                                <Link className="btn btn-secondary margin-left-10" to="/">Home</Link>
-                                <Link className="btn btn-secondary margin-left-10" to="/about">About</Link>
-                                <Link className="btn btn-secondary margin-left-10" to="/new">Post</Link>
-                                <Link className="btn btn-secondary margin-left-10" to="/upload">Upload</Link>
-                        </Row>
-                    </Grid>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/about" component={About}/>
-                    <Route path="/new" component={Photos}/>
-                    <Route path="/upload" component={Video}/>
-                </Router>
-                <div>
-                   <Photos/>
-                </div>
-
-            </React.Fragment>
-        );
+  render() {
+    const { photos } = this.props;
+    return (
+      <React.Fragment>
+        <Router>
+          <Grid fluid>
+            <Row
+              className="wtf center-xs"
+              style={{ "justify-content": "center", "text-align": "center" }}
+            >
+              <Link className="btn btn-secondary margin-left-10" to="/">
+                Home
+              </Link>
+              <Link className="btn btn-secondary margin-left-10" to="/about">
+                About
+              </Link>
+              <Link className="btn btn-secondary margin-left-10" to="/new">
+                Post
+              </Link>
+              <Link className="btn btn-secondary margin-left-10" to="/upload">
+                Upload
+              </Link>
+            </Row>
+          </Grid>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/new" component={Photos} />
+          <Route path="/upload" component={Video} />
+        </Router>
+        <div>
+          <Photos />
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
-
-function Home (props) {
-  return(
-    <React.Fragment>
-    </React.Fragment>
-  );
+function Home(props) {
+  return <React.Fragment></React.Fragment>;
 }
 
 MainApp.propTypes = {
   photos: PropTypes.array.isRequired,
   message: PropTypes.string,
-  dispatch: PropTypes.func.isRequired
+  dispatch: PropTypes.func.isRequired,
 };
 
-function mapStateToProps ( state ) {
-    const { photos } = state
-    return {
-        photos
-    }
+function mapStateToProps(state) {
+  const { photos } = state;
+  return {
+    photos,
+  };
 }
 
-const mapDispatchToProps = dispatch => {
-    return {
-        delete: () =>  dispatch({type: 'DELETE_PHOTO'}),
-        add: () => dispatch({type: 'ADD_PHOTO'}),
-        dispatch: (param) => dispatch(param)
-    }
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    delete: () => dispatch({ type: "DELETE_PHOTO" }),
+    add: () => dispatch({ type: "ADD_PHOTO" }),
+    dispatch: (param) => dispatch(param),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(MainApp)
-
+export default connect(mapStateToProps, mapDispatchToProps)(MainApp);
--- app/javascript/components/Root.js
+++ app/javascript/components/Root.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import MainApp from './MainApp'
-import PhotoReducer from  './PhotoReducer'
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import MainApp from "./MainApp";
+import PhotoReducer from "./PhotoReducer";
 
-import {createStore, applyMiddleware, compose} from "redux"
-import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
+import { createStore, applyMiddleware, compose } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
 
-const loggerMiddleware = createLogger()
+const loggerMiddleware = createLogger();
 export const store = createStore(
-    PhotoReducer,
-    {photos: []}, compose(applyMiddleware(thunkMiddleware), window.devToolsExtension ? window.devToolsExtension() : f => f))
+  PhotoReducer,
+  { photos: [] },
+  compose(
+    applyMiddleware(thunkMiddleware),
+    window.devToolsExtension ? window.devToolsExtension() : (f) => f
+  )
+);
 
 export default class Root extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <MainApp />
-            </Provider>
-        )
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <MainApp />
+      </Provider>
+    );
+  }
 }
